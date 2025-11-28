a2ee41deac530d806b8f7c342306ba77f279e85b
nuxl0x <nuclearx86@gmail.com>
2025-11-28 21:48:40 +1100

    Added functionality to all binding items.

--- src/main/kotlin/nux/enchained/util/itypes/BoundVowItem.kt
+++ src/main/kotlin/nux/enchained/util/itypes/BoundVowItem.kt
@@ -1,7 +1,32 @@
 package nux.enchained.util.itypes
 
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
-import net.minecraft.item.Item.Settings
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
 
 class BoundVowItem(settings: Settings) : Item(settings) {
+
+    override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
+        val nbt: NbtCompound = stack.orCreateNbt
+
+        tooltip.add(Text.translatable("itemTooltip.enchained.bVow1"))
+        tooltip.add(Text.translatable("itemTooltip.enchained.bVow2"))
+
+        // value updates from nbt
+        if (nbt.contains("primaryUser") && nbt.contains("secondaryUser")) {
+            val primaryUser: String = nbt.getString("primaryUser")
+            val secondaryUser: String = nbt.getString("secondaryUser")
+
+            tooltip.add(Text.translatable("itemTooltip.enchained.primaryUser", primaryUser).formatted(Formatting.GOLD))
+            tooltip.add(Text.translatable("itemTooltip.enchained.secondaryUser", secondaryUser).formatted(Formatting.DARK_RED))
+        }
+
+        super.appendTooltip(stack, world, tooltip, context)
+
+    }
+
 }
